57a9394d3df5609de5a4312857049715c71ea96c
tayyaba142begum <134700360+tayyaba142begum@users.noreply.github.com>
2023-06-10 14:54:58 +0530

    Add files via upload

---
+++ example_python.py
@@ -0,0 +1,14 @@
+receipe = """
+1. Heat the pan and add oil
+2. Crack the egg
+3. Add salt in egg and mix well
+4. Pour the mixture in pan
+5. Fry on medium heat
+"""
+print(receipe)
+
+note = '''
+This is a multiline string
+It is used to display multiline message in the program
+'''
+print(note)
